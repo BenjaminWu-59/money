8718f99583860f2bcd888fe18e2b5f391c38e446
Benjamin <wuqiong3699@gmail.com>
2021-08-12 11:54:54 +0800

    Detail.vue 对数据进行排列

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -26,6 +26,7 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import intervalList from '@/constants/intervalList';
 import dayjs from 'dayjs';
+import clone from '@/lib/clone';
 
 @Component({
   components: {Types,Tabs}
@@ -40,15 +41,34 @@ export default class Detail extends Vue {
   }
   get result() {
     const {recordList} = this;
-    type HashTableValue = { title: string, items:RecordItem[]}
-
-    const hashTable: { [key: string]: HashTableValue } = {};
-    for (let i = 0; i < recordList.length; i++) {
-      const [date, time] = recordList[i].createAt!.split('T');
-      hashTable[date] = hashTable[date] || {title: date, items: []};
-      hashTable[date].items.push(recordList[i]);
+    // type HashTableValue = { title: string, items:RecordItem[]}
+    if (recordList.length === 0) {
+      return [];
     }
-    return hashTable;
+    // const hashTable: {title: string,items:RecordItem[]}[];
+    const newList = clone(recordList)
+        .filter(r => r.type === this.type)
+        .sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());//对recordList进行排序，不排序时间就会乱展示
+
+    type Result = { title: string, total?: number, items: RecordItem[] }[]
+    const result: Result = [{title: dayjs(newList[0].createAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createAt), 'day')) {
+        last.items.push(current);
+      } else {
+        result.push({title: dayjs(current.createAt).format('YYYY-MM-DD'), items: [current]});
+      }
+    }
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => {
+        console.log(sum);
+        console.log(item);
+        return sum + item.amount;
+      }, 0);
+    });
+    return result;
   }
 
   beautify(string:string){
